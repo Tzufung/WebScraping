3a9524054e46448cfaf8200fb9a0350b8a802132
Tzufung <zfchen1314@gmail.com>
2017-06-20 18:09:48 +0800

    urlopen_timeout_error

--- Urllib/urlopen_timeout.py
+++ Urllib/urlopen_timeout.py
@@ -1,4 +1,10 @@
+# 设置过短的响应时间，导致异常
 import urllib.request
+import urllib.error
+import socket
 
-response = urllib.request.urlopen("http://httpbin.org/get", timeout=1)
-print(response.read())
+try:
+    response = urllib.request.urlopen("http://httpbin.org/get", timeout=0.1)
+except urllib.error.URLError as e:
+    if isinstance(e.reason, socket.timeout):
+        print("TIME OUT")
