6a639d2b03ddc8018db63c23a3dc39b6f558df67
Tzufung <zfchen1314@gmail.com>
2017-06-20 21:24:01 +0800

    Cookie维持登陆信息

---
+++ Urllib/Cookie.py
@@ -0,0 +1,9 @@
+# Cookie维持登陆信息
+import http.cookiejar, urllib.request
+
+cookie = http.cookiejar.CookieJar()
+handler = urllib.request.HTTPCookieProcessor(cookie)
+opener = urllib.request.build_opener(handler)
+response = opener.open("http://www.baidu.com")
+for item in cookie:
+    print(item.name+"="+item.value)
