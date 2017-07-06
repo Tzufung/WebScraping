476f4506f39c7887b2307f84e529a39c509621bb
Tzufung <zfchen1314@gmail.com>
2017-07-06 21:40:10 +0800

    .*泛匹配

--- re/match1.py
+++ re/match1.py
@@ -3,9 +3,15 @@
 import re
 
 content = "Hello 123 4567 World_This is a Regex Demo"
+
 result = re.match("Hello", content)
 print(result)
 print(result.group())
+
 result2 = re.match("^Hello\s\d\d\d\s\d{4}\s\w{10}.*Demo$", content)
 print(result2)
 print(result2.group())
+
+result3 = re.match("^Hello.*Demo$", content)
+print(result3)
+print(result3.group())
